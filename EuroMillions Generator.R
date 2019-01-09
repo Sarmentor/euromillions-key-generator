072caa31d358fac1a9a4a7ca33d4d1fb4e3c9fb6
Rodrick Sarmento <Sarmentor@users.noreply.github.com>
2019-01-09 22:29:40 +0000

    Update EuroMillions Generator.R

--- EuroMillions Generator.R
+++ EuroMillions Generator.R
@@ -5,21 +5,27 @@ generator <<- function(n, min, max){
   return(nums)
 }
 
-my.stars <<- function(){
-  return(generator(2,1,12))
+my.stars <<- function(nstars){
+  return(generator(nstars,1,12))
 }
 
-my.numbers <<- function(){
-  return(generator(5,1,50))
+my.numbers <<- function(nnum){
+  return(generator(nnum,1,50))
 }
 
 go <- function(nkeys){
   print("Here are the keys generated:")
   for(key in 1:nkeys){
-   num <- my.numbers()
-   st <- my.stars()
+   num <- my.numbers(nnum)
+   st <- my.stars(nstars)
    cat(paste(num, collapse=" ")," + ",paste(st,collapse = " "),"\n", sep="")
   }
 }
 
-go(1)
+
+cat("Intro: Are you ready?\nYou will be asked how many keys you want to generate,\nhow many numbers and stars you want the keys to have!")
+nkeys <<- readline("What is the quantity of keys you want?")
+nnum <<- readline("What is the quantity of numbers you want?")
+nstars <<- readline("What is the quantity of stars you want?")
+
+go(nkeys)
